431003b61e1179c20e53e7e50151fd908e9618e6
jihanlugas <jihanlugas2@gmail.com>
2023-03-26 22:16:38 +0700

    blog cara install nodejs

--- src/utils/Helper.ts
+++ src/utils/Helper.ts
@@ -1,4 +1,4 @@
-import { isEmpty, isEmptyObject, typeCheck } from '@utils/Validate';
+import { isEmpty, isEmptyObject, typeCheck } from '@/utils/Validate';
 import { v4 as uuidv4 } from 'uuid';
 
 export const convertJsonData = (json) => {
